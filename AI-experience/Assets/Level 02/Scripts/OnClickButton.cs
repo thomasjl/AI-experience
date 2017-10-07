1e7a4a5a2a7dcd3d42a6b57dcffa4e7a4e82bc9d
maudetes <maudet.estelle@gmail.com>
2017-10-07 17:33:05 +0200

    Create Level 02 basics

---
+++ AI-experience/Assets/Level 02/Scripts/OnClickButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickButton : MonoBehaviour {
+
+	public Text text;
+
+	public void onClickTest() {
+		text.text = "Choix 1";
+	}
+}

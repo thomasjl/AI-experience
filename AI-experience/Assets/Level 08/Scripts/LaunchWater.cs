1e4c7d4fcaae812d4f6b67b34dccbb759e5fd9f3
Thomas JEAN-LOUIS <thomjl78@gmail.com>
2017-10-10 23:37:01 +0200

    add buildings, interactible objects

---
+++ AI-experience/Assets/Level 08/Scripts/LaunchWater.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchWater : MonoBehaviour {
+
+    public GameObject water;
+    private bool hasBeenPushed;
+
+    void Start()
+    {
+        hasBeenPushed = false;
+    }
+
+    void OnCollisionEnter( Collision collision)
+    {
+        if (collision.gameObject.tag == "Camaro" && hasBeenPushed==false)
+        {
+            Debug.Log("percussion");
+            Instantiate(water, transform.position, Quaternion.identity);
+            hasBeenPushed = true;
+        }
+      
+    }
+}

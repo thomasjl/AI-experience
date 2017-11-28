f25895bdeda6c22529a2a4a6e1d636feef79399f
raphaelhamonnais <raphael.hamonnais@gmail.com>
2017-11-28 11:13:39 +0100

    Level 1 - version 1

---
+++ AI-experience/Assets/Level 01/Scripts/HandOnMouseOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandOnMouseOver : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+		
+	public void ChangeCursorToHand() {
+		Cursor.SetCursor (GameControlLevel1.instance.hand_cursor , Vector2.zero, CursorMode.Auto);
+	}
+
+	public void ChangeCursorToNormal() {
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+	}
+
+}
